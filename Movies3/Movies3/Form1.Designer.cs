f2575406ada88d9c26d459a8f6670f42514af3e5
dannymccaslin <mccaslin.danny@gmail.com>
2016-01-12 23:44:21 -0500

    Movie Lister
    This program allows users to list movies they've seen and when they've
    seen them in a Data Table. I'm writing this to learn C#.

---
+++ Movies3/Movies3/Form1.Designer.cs
@@ -0,0 +1,209 @@
+﻿namespace Movies3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.submitButton = new System.Windows.Forms.Button();
+            this.movieTitle = new System.Windows.Forms.TextBox();
+            this.movieDate = new System.Windows.Forms.DateTimePicker();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.saveButton = new System.Windows.Forms.Button();
+            this.loadButton = new System.Windows.Forms.Button();
+            this.searchButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.movieSearch = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.deleteButton = new System.Windows.Forms.Button();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // submitButton
+            // 
+            this.submitButton.Location = new System.Drawing.Point(181, 114);
+            this.submitButton.Name = "submitButton";
+            this.submitButton.Size = new System.Drawing.Size(75, 23);
+            this.submitButton.TabIndex = 0;
+            this.submitButton.Text = "Submit";
+            this.submitButton.UseVisualStyleBackColor = true;
+            this.submitButton.Click += new System.EventHandler(this.submitButton_Click);
+            // 
+            // movieTitle
+            // 
+            this.movieTitle.Location = new System.Drawing.Point(22, 88);
+            this.movieTitle.Name = "movieTitle";
+            this.movieTitle.Size = new System.Drawing.Size(200, 20);
+            this.movieTitle.TabIndex = 1;
+            // 
+            // movieDate
+            // 
+            this.movieDate.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.movieDate.Location = new System.Drawing.Point(22, 32);
+            this.movieDate.Name = "movieDate";
+            this.movieDate.Size = new System.Drawing.Size(200, 20);
+            this.movieDate.TabIndex = 2;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(316, 17);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(396, 340);
+            this.dataGridView1.TabIndex = 3;
+            // 
+            // saveButton
+            // 
+            this.saveButton.Location = new System.Drawing.Point(316, 363);
+            this.saveButton.Name = "saveButton";
+            this.saveButton.Size = new System.Drawing.Size(75, 23);
+            this.saveButton.TabIndex = 4;
+            this.saveButton.Text = "Save";
+            this.saveButton.UseVisualStyleBackColor = true;
+            this.saveButton.Click += new System.EventHandler(this.saveButton_Click);
+            // 
+            // loadButton
+            // 
+            this.loadButton.Location = new System.Drawing.Point(316, 392);
+            this.loadButton.Name = "loadButton";
+            this.loadButton.Size = new System.Drawing.Size(75, 23);
+            this.loadButton.TabIndex = 5;
+            this.loadButton.Text = "Load";
+            this.loadButton.UseVisualStyleBackColor = true;
+            this.loadButton.Click += new System.EventHandler(this.loadButton_Click);
+            // 
+            // searchButton
+            // 
+            this.searchButton.Location = new System.Drawing.Point(181, 275);
+            this.searchButton.Name = "searchButton";
+            this.searchButton.Size = new System.Drawing.Size(75, 23);
+            this.searchButton.TabIndex = 6;
+            this.searchButton.Text = "Search";
+            this.searchButton.UseVisualStyleBackColor = true;
+            this.searchButton.Click += new System.EventHandler(this.searchButton_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(19, 232);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Search";
+            // 
+            // movieSearch
+            // 
+            this.movieSearch.Location = new System.Drawing.Point(22, 249);
+            this.movieSearch.Name = "movieSearch";
+            this.movieSearch.Size = new System.Drawing.Size(200, 20);
+            this.movieSearch.TabIndex = 8;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(22, 17);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(62, 13);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Movie Date";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(22, 72);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(67, 13);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Movie Name";
+            // 
+            // deleteButton
+            // 
+            this.deleteButton.Location = new System.Drawing.Point(637, 363);
+            this.deleteButton.Name = "deleteButton";
+            this.deleteButton.Size = new System.Drawing.Size(75, 23);
+            this.deleteButton.TabIndex = 11;
+            this.deleteButton.Text = "Delete";
+            this.deleteButton.UseVisualStyleBackColor = true;
+            this.deleteButton.Click += new System.EventHandler(this.deleteButton_Click);
+            // 
+            // saveFileDialog1
+            // 
+            this.saveFileDialog1.DefaultExt = "xml";
+            this.saveFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.saveFileDialog1_FileOk);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.DefaultExt = "xml";
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog1_FileOk);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(728, 418);
+            this.Controls.Add(this.deleteButton);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.movieSearch);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.searchButton);
+            this.Controls.Add(this.loadButton);
+            this.Controls.Add(this.saveButton);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.movieDate);
+            this.Controls.Add(this.movieTitle);
+            this.Controls.Add(this.submitButton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button submitButton;
+        private System.Windows.Forms.TextBox movieTitle;
+        private System.Windows.Forms.DateTimePicker movieDate;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button saveButton;
+        private System.Windows.Forms.Button loadButton;
+        private System.Windows.Forms.Button searchButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox movieSearch;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button deleteButton;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+    }
+}
+
---
+++ Movies3/Movies3/Form1.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Movies3
+{
+    public partial class Form1 : Form
+    {
+        //create a list for my columns
+        List<string> _names = new List<string>();
+
+        List<string> _dates = new List<string>();
+        List<string> _titles = new List<string>();
+        DataSet ds = new DataSet();
+        DataTable dt = new DataTable();
+
+
+
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            //Add columns
+            _names.Add("Date");
+            _names.Add("Movie Title");
+
+           
+
+            for (int i = 0; i < this._names.Count; i++)
+            {
+                string name = this._names[i];
+                dt.Columns.Add(name);
+
+
+            }
+            ds.Tables.Add(dt);
+            //Render the DataGridView
+            dataGridView1.DataSource = dt;
+            dataGridView1.Columns["Movie Title"].Width = 200;
+            dataGridView1.MultiSelect = false;
+
+            openFileDialog1.Filter = "xml files (*.xml)|*.xml";
+        }
+
+  
+        public void submitButton_Click(object sender, EventArgs e)
+        {
+
+
+            if (movieTitle.Text != null && movieTitle.Text != "")
+            {
+                DataRow dtRow;
+                dtRow = dt.NewRow();
+                dtRow["Date"] = movieDate.Text;
+                dtRow["Movie Title"] = movieTitle.Text;
+                dt.Rows.Add(dtRow);
+            }
+            else
+            {
+                MessageBox.Show("You need to type a movie title to save a movie to your list.");
+            }
+            movieTitle.Text = null;
+
+            dataGridView1.DataSource = dt;
+        }
+
+        private void saveButton_Click(object sender, EventArgs e)
+        {
+            saveFileDialog1.ShowDialog();
+        }
+
+        private void loadButton_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.ShowDialog();
+
+        }
+
+        public void deleteRow(int i)
+        {
+
+            DataRow dr = dt.Rows[i];
+            dr.Delete();
+
+        }
+        private void searchButton_Click(object sender, EventArgs e)
+        {
+
+            /*   
+
+           int index = dt.Rows.IndexOf(foundRow[0]);
+           */
+
+            for (int i = 0; i < dt.Rows.Count; i++)
+             {
+
+                if (Convert.ToString(dt.Rows[i]["Movie Title"]) == movieSearch.Text)
+                 {
+                     
+                     dataGridView1.Rows[i].Selected = true;
+                 } 
+
+
+             }
+        }
+
+        private void deleteButton_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("Are You Sure?", "Confirm", MessageBoxButtons.YesNo, MessageBoxIcon.Question)== DialogResult.Yes)
+            {
+                // Do stuff if yes
+                //Get the selected row by looping through all of the rows and finding one that has been selected
+                for (int i = 0; i < dt.Rows.Count; i++)
+                {
+                    if (dataGridView1.Rows[i].Selected == true)
+                    {
+                        deleteRow(i);
+                    }
+                    
+                }
+            }
+            else
+            {
+                // Close the window
+            }
+        }
+
+        private void saveFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+            string name = saveFileDialog1.FileName;
+
+            ds.WriteXml(name);
+        }
+
+        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+            string name = openFileDialog1.FileName;
+
+            ds.ReadXml(name);
+        }
+    }
+}
+
